43d137d3cc68293a4ff1d2986bf7ec045103d774
Elena Semeshyna <dp030492sos@gmail.com>
2021-12-14 12:22:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import { Component } from 'react';
-// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import Loader from 'react-loader-spinner';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 // import * as Scroll from 'react-scroll';
+import { animateScroll as scroll } from 'react-scroll';
 import Searchbar from './components/Searchbar';
 import ImageGallery from './components/ImageGallery';
 import LoadMore from './components/Button';
 import API from './services/image-api.js';
 import Modal from './components/Modal';
 import './App.css';
-
-//State machine
-//Scroll gallery
-//Refactoring
 
 class App extends Component {
   state = {
@@ -25,6 +21,48 @@ class App extends Component {
     showModal: false,
     modalImage: null,
     modalAlt: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { searchQuery, page } = this.state;
+
+    if (prevState.searchQuery !== searchQuery) {
+      this.setState({ status: 'pending' });
+
+      API(searchQuery, page)
+        .then(response =>
+          this.setState({
+            imageArr: response.hits,
+            page: 1,
+            status: 'resolved',
+          }),
+        )
+        .catch(error => this.setState({ error, status: 'rejected' }));
+    }
+
+    if (prevState.page !== page && page !== 1) {
+      this.setState({ status: 'pending' });
+
+      API(searchQuery, page)
+        .then(
+          response =>
+            this.setState(prevState => ({
+              imageArr: [...prevState.imageArr, ...response.hits],
+            })),
+          this.setState({ status: 'resolved' }),
+        )
+        .catch(error => this.setState({ error, status: 'rejected' }));
+      scroll.scrollToBottom(200);
+    }
+  }
+
+  handleFormSubmit = searchQuery => {
+    this.setState({ imageArr: [], searchQuery, page: 1 });
+  };
+  onLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
   };
 
   openModal = arrayEl => {
@@ -44,102 +82,17 @@ class App extends Component {
     });
   };
 
-  componentDidMount() {
-    toast.info('Find image');
-    console.log(API);
-  }
-
-  async componentDidUpdate(prevProps, prevState) {
-    const { searchQuery, page } = this.state;
-    // const response = await API(searchQuery, page);
-
-    if (prevState.searchQuery !== searchQuery) {
-      this.setState({ status: 'pending' });
-
-      // try {
-      //   if (response.hits.length > 0) {
-      //     this.setState({
-      //       imageArr: response.hits,
-      //       page: 1,
-      //       status: 'resolved',
-      //     });
-      //     toast.success('Images found');
-      //   }
-
-      //   if (response.hits.length === 0) {
-      //     this.setState({ status: 'idle' });
-      //     toast.error('No images found');
-      //   }
-      // } catch (error) {
-      //   this.setState({ error, status: 'rejected' });
-      // }
-
-      // try {
-      //   const response = await API(searchQuery, page);
-
-      //   const { hits } = response;
-
-      //   if (hits.length === 0) {
-      //     toast.error('No images found');
-      //     this.setState({ status: 'idle' });
-      //   } else if (hits.length > 0) {
-      //     toast.success('images found');
-      //     this.setState({ imageArr: hits, status: 'resolved' });
-      //   }
-      // } catch (error) {
-      //   toast.error('something went wrong');
-      //   return this.setState({ error: error, status: 'rejected' });
-      // }
-
-      API(searchQuery, page)
-        .then(
-          response =>
-            this.setState({
-              imageArr: response.hits,
-              page: 1,
-              status: 'resolved',
-            }),
-          toast.success(`Images with ${this.state.searchQuery} found`),
-        )
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
-
-    if (prevState.page !== this.state.page) {
-      this.setState({ status: 'pending' });
-
-      API(searchQuery, page)
-        .then(
-          response =>
-            this.setState(prevState => ({
-              imageArr: [...prevState.imageArr, ...response.hits],
-            })),
-          this.setState({ status: 'resolved' }),
-        )
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
-  }
-
-  onLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  handleFormSubmit = searchQuery => {
-    this.setState({ imageArr: [], searchQuery, page: 1 });
-  };
-
   render() {
     const { imageArr, showModal, modalImage, modalAlt, status, error } =
       this.state;
     return (
       <div className="App">
-        <ToastContainer autoClose={3000} />
         <Searchbar submit={this.handleFormSubmit} />
 
         {status === 'resolved' && (
           <>
             <ImageGallery imageArray={imageArr} onImgClick={this.openModal} />
+            {imageArr.length === 0 && <h1>No images found</h1>}
             {imageArr.length >= 12 && (
               <LoadMore fetchMoreImg={this.onLoadMore} />
             )}
